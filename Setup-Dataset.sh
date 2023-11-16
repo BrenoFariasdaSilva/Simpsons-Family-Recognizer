5ec211f865cafe346bf10be912e9ad249ac41cf8
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-11-16 17:12:56 -0300

    REFACTOR: Updating the Setup Dataset ShellScript

--- Setup-Dataset.sh
+++ Setup-Dataset.sh
@@ -4,19 +4,45 @@
 # chmod +x ./Setup-Dataset.sh
 # ./Setup-Dataset.sh
 
-# URL of the zip file on Google Drive
-file_url="https://drive.google.com/uc?export=download&id=1wVyUmsz150uKjOprxRA_4LtmTXDPRp1o"
-
-# Output zip file name
+dataset_dir="./Dataset"
 zip_file="Simpsons-Dataset.zip"
 
-# Download the file using wget
-wget "$file_url" -O "$zip_file"
+# Clean up: Remove the downloaded zip file
+cleanup() {
+	rm "$zip_file"
+}
 
-# Extract the contents
-unzip $zip_file
+# Verify that the dataset has not already been downloaded
+verify_dataset() {
+	if [ -d "$dataset_dir" ]; then
+		echo "Dataset already exists. Removing it first."
+		rm -rf "$dataset_dir"
+	fi
+}
 
-# Clean up: remove the downloaded zip file
-rm $zip_file
+# Download the dataset
+download_dataset() {
+	# URL of the zip file on Google Drive
+	file_url="https://drive.google.com/uc?export=download&id=1wVyUmsz150uKjOprxRA_4LtmTXDPRp1o"
 
-echo "Download and extraction complete."
+	# Download the file using wget
+	wget "$file_url" -O "$zip_file"
+}
+
+# Extract the zip file
+extract_dataset() {
+	unzip "$zip_file"
+}
+
+# Main function
+main() {
+	verify_dataset # Check if dataset already exists
+	download_dataset # Download the dataset
+	extract_dataset # Extract the dataset
+	cleanup # Remove the downloaded zip file
+
+	echo "Download and extraction complete."
+}
+
+# Run the main function
+main
